0a49b6de7e51ff0e451fca8dda9612a0aa4c5e73
Sanskar <manojb4u.2007@gmail.com>
2025-12-26 12:50:36 +0530

    status updated

--- routes/user.py
+++ routes/user.py
@@ -31,7 +31,7 @@ async def register(user: User,userinfo = Depends(role_required([Role.Admin,Role.
     cur = execute_company_query(db_query['USER']['SELECT_USER_NAME'], username)
     if cur:
          return Response(
-            status=True,
+            status=False,
             code=200,
             message="Username already exists",
             data=[]
