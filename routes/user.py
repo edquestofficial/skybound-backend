24426577284009f5f4fc18db28ce724271be36b8
Sanskar <manojb4u.2007@gmail.com>
2026-01-13 11:15:13 +0530

    remove iat

--- routes/user.py
+++ routes/user.py
@@ -118,8 +118,7 @@ def verify_password(plain_pass: str, hashed_pass: str) -> bool:
     return bcrypt.checkpw(plain_pass.encode(), hashed_pass.encode())
 
 def create_token(userDetails:User):
-    payload = {"id":userDetails["id"], "name":userDetails["name"],"role":userDetails["role"],
-               "iat": datetime.utcnow()}
+    payload = {"id":userDetails["id"], "name":userDetails["name"],"role":userDetails["role"]}
     return jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
 
 
